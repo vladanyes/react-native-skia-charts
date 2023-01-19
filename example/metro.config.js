2f0de2e1a1a76c328c9ca8886c2b8b33d8ce239e
vladr <vladislav@usmobile.com>
2023-01-19 17:44:52 +0600

    fix example bug

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const escape = require('escape-string-regexp');
-const { getDefaultConfig } = require('@expo/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
+const escape = require('escape-string-regexp');
 const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
@@ -10,19 +9,13 @@ const modules = Object.keys({
   ...pak.peerDependencies,
 });
 
-const defaultConfig = getDefaultConfig(__dirname);
-
 module.exports = {
-  ...defaultConfig,
-
-  projectRoot: __dirname,
-  watchFolders: [root],
+  projectRoot: path.resolve('example'),
+  watchFolders: [path.resolve('node_modules')],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we block them at the root, and alias them to the versions in example's node_modules
+  // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    ...defaultConfig.resolver,
-
     blacklistRE: exclusionList(
       modules.map(
         (m) =>
@@ -34,5 +27,14 @@ module.exports = {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
     }, {}),
+  },
+
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
 };
